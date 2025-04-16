06fe3a32ea85b38ce223b988a94d0fae788a776c
najwakussyafira <kussyafira@gmail.com>
2025-04-16 12:47:33 +0700

    Add files via upload

---
+++ CaseMethodKel1/MataKuliah.java
@@ -0,0 +1,18 @@
+package CaseMethodKel1;
+
+public class MataKuliah {
+    
+String kodeMK;
+String namaMK;
+int sks;
+
+public MataKuliah(String kodeMK, String namaMK, int sks) {
+    this.kodeMK = kodeMK;
+    this.namaMK = namaMK;
+    this.sks = sks;
+}
+
+public void tampilMataKuliah() {
+    System.out.println("Kode MK: " + kodeMK + " | Nama: " + namaMK + " | SKS: " + sks);
+}
+}
